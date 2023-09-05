fe69bf5c6bdffa2d7a51b198dd6bd21c205aa671
FuCanCode <fustef@outlook.de>
2023-09-05 10:05:59 +0200

    countdown and sound enabled

--- public/app.js
+++ public/app.js
@@ -1,6 +1,20 @@
 "use strict";
 const btnStart = document.querySelector(".btn__start");
 const timerEl = document.querySelector(".timer");
+const finishSound = new Audio("paiste-gong-75913.mp3");
 const timer = function (min = 25) {
-    setInterval(() => { }, 1000, min);
+    let seconds = min * 60;
+    const countdown = () => {
+        if (seconds === 0) {
+            finishSound.play();
+            clearInterval(timerId);
+        }
+        timerEl.textContent = new Intl.DateTimeFormat(navigator.language, { minute: "2-digit", second: "2-digit" }).format(seconds * 1000) + " min";
+        seconds--;
+    };
+    const timerId = setInterval(countdown, 1000, min);
 };
+btnStart.addEventListener("click", (ev) => {
+    console.log(ev);
+    timer();
+});
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 const btnStart = document.querySelector(".btn__start") as HTMLDivElement;
 const timerEl = document.querySelector(".timer") as HTMLParagraphElement;
 
+const finishSound = new Audio("paiste-gong-75913.mp3");
+
 const timer = function (min: number = 25) {
-  setInterval(() => {}, 1000, min);
+  let seconds = min * 60;
+
+  const countdown = () => {
+    if (seconds === 0) {
+      finishSound.play();
+      clearInterval(timerId);
+    }
+
+    timerEl.textContent = new Intl.DateTimeFormat(navigator.language, { minute: "2-digit", second: "2-digit" }).format(seconds * 1000) + " min";
+    seconds--;
+  };
+
+  const timerId = setInterval(countdown, 1000, min);
 };
+
+btnStart.addEventListener("click", (ev) => {
+  console.log(ev);
+  timer();
+});
