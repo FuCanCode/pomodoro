7b2c1b4fe65d21476e7d348dd01ae8f42cf7f814
Stefan F. <fustef@outlook.de>
2024-03-14 13:14:42 +0100

    create working react version

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+const defaultMinutes = 0.2;
+const finishSound = new Audio("paiste-gong-75913.mp3");
+finishSound.volume = 0.4;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [timeLeft, setTimeLeft] = useState(defaultMinutes * 60);
+
+  const interval = useRef<null | number>(null);
+
+  const minutes = Math.trunc(timeLeft / 60);
+  const seconds = timeLeft % 60;
+  const displayTime =
+    timeLeft > 0
+      ? `${minutes}:${seconds < 10 ? "0" + seconds : seconds} min`
+      : "Take a break!";
+
+  useEffect(() => {
+    if (timeLeft <= 0 && interval.current) {
+      finishSound.play();
+      clearInterval(interval.current);
+      interval.current = null;
+      document.title = displayTime;
+      return;
+    }
+
+    document.title =
+      timeLeft > 0 && interval.current ? displayTime : "Pomodoro";
+  }, [timeLeft, displayTime]);
+
+  const startTimer = () => {
+    if (interval.current) clearInterval(interval.current);
+
+    setTimeLeft(defaultMinutes * 60);
+
+    interval.current = setInterval(() => {
+      setTimeLeft((prevTime) => prevTime - 1);
+    }, 1000);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={startTimer} className="logo">
+        Start being focused!
+      </button>
+      <p>{displayTime}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
